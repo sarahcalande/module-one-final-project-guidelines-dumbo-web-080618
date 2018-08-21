531130bce92fa79ece678a4cc2d939053030719d
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-08-21 18:43:08 -0400

    stuff

--- bin/run.rb
+++ bin/run.rb
@@ -3,3 +3,12 @@ require_relative '../config/environment'
 
 
 puts "HELLO WORLD"
+
+
+def main
+    i = TTY::Prompt.new.select("What would you like to do?") do |menu|
+        menu.choices Login: "login", "Sign Up" => "signup", Exit: "exit"
+    end
+end
+
+main
