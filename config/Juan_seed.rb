4b63b782cdacdc9f4b71640d0b99cc30f4b4ebfc
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-08-21 16:16:42 -0400

    Done

---
+++ config/Juan_seed.rb
@@ -0,0 +1,15 @@
+# Sports
+
+mets = Activity.create(name:"Sports", place: "Citi Field", genre: "Baseball", price: 50, time:1915, date: ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"])
+nets = Activity.create(name:"Sports", place: "Barclay Center", genre: "Basketball", price: 70, time:2000, date: ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"])
+knicks = Activity.create(name:"Sports", place: "Madison Square Garden", genre: "Basketball", price: 30, time:2000, date: ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"])
+islanders = Activity.create(name:"Sports", place: " Nassau Veterans Memorial Coliseum", genre: "Hockey", price: 80, time:1800, date: ["Friday","Saturday","Sunday"])
+giants = Activity.create(name:"Sports", place: "MetLife Stadium", genre: "Football", price: 80, time:, date: ["Thursday","Sunday"])
+
+
+#Dancing
+house = Activity.create(name:"Dancing", place:"House of Yes", genre: "House", price: 30, time:1800, date: ["Friday,Saturday"])
+latin = Activity.create(name:"Dancing", place:"Bembe", genre: "Latin", price: 30, time:2200, date: ["Friday,Saturday"])
+mumble_rap = Activity.create(name:"Dancing", place:"40/40 Club", genre: "Mumble Rap", price: 50, time:2200, date: ["Friday,Saturday"])
+salsa = Activity.create(name:"Dancing", place:"Club Cache", genre: "Salsa", price: 40, time:2200, date: ["Friday,Saturday"])
+mixture = Activity.create(name:"Dancing", place:"Output", genre: "Everything", price: 50, time:2000, date: ["Friday,Saturday"])
