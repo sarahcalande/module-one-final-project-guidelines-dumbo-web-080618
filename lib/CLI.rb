bd690e1ea9da298ab2dd4e12f3bb465d678e25d8
github username <github email address>
2018-08-21 18:20:46 -0400

    messed around with tty

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,6 +1,42 @@
+#######################
+
+#"Heard from a friend"
+# "Welcome to heard from a friend"
+# => "do you have a profile with us?"
+# =>   n - sign up with your email/name/price cap? (other attributes)
+# =>   y - would you like to see your favorited events or do you want us to suggest something?
+  #new method, interface
+
+#new method ***needs name****
+# what do you want to do? Enter any of the following
+    # day of the week
+    # kind of event
+    # price cap
+    #
+
+
+
+
+############ fix in seed ###############
+
+#wrap signle days in arrays for iteration purposes
+
+
 
 def greeting
-  prompt = TTY::Prompt.new.yes?("Do you have a profile?")
+  prompt = TTY::Prompt.new.yes?("Welcome to Heard From a Friend, do you have a profile?")
+
+
+    if prompt == true
+      puts "good"
+      #What is your email?
+    elsif prompt == false
+      puts "bad"
+      #Create User instance
+    elsif prompt != true && prompt !=  false
+      puts "ok"
+    end
+
 end
 
 # def greeting
