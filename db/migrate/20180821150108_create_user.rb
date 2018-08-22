e99ed3bf54e7838c459579dee46abe1ad5d9fea7
github username <github email address>
2018-08-22 14:55:05 -0400

    cleaned up interface/deleted irrelevant columns

--- db/migrate/20180821150108_create_user.rb
+++ db/migrate/20180821150108_create_user.rb
@@ -2,7 +2,6 @@ class CreateUser < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.integer :max_price_range
       t.string :email
     end
   end
